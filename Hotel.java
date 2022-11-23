fed853b9aabed5cba64da73036d7996c7991938d
Your Name <mohamednaveedmass@gmail.com>
2022-11-23 11:25:41 +0530

    home

---
+++ Hotel.java
@@ -0,0 +1,32 @@
+package lk.abcd.demoae2project.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+
+public class Hotel {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @Column(nullable = false,length = 50)
+    private String name;
+
+    @Column
+    private String addressline1;
+
+    @Column
+    private String addressline2;
+
+    @Column
+    private String country;
+
+    @Column
+    private String numberofemployees;
+}
